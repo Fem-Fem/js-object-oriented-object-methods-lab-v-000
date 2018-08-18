c48ff21cdc20acdb8ab4058337c24085ce99f2db
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-18 22:14:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function　BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
-  this.veto = function {
+  this.veto = function() {
     return "No, I must disagree"
   }
 }
